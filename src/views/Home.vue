83d139f62b2d627ad708e7d53a91e0151e0e459c
Kipngetich <khalifngeno@gmail.com>
2021-06-08 09:33:45 +0300

    add initial code  to vuejs weather app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" >
+    <div class="row">
+      <div class="col-md-3" id="sidepanel">
+        <SidePanel msg="Welcome to Your Vue.js App"/>
+      </div>
+      <div class="col-md-9">
+        <h1> Main Panel section </h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import SidePanel from '@/components/SidePanel.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    SidePanel,
   },
 };
 </script>
+<style scoped>
+</style>
